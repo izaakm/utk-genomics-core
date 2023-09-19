380367cc86bcde3b613e08704d8dcf1fceeaf48e
John Miller <johnmiller@macbook-103b1.device.utk.edu>
2023-09-19 11:15:41 -0400

    Log submissions to file

--- src/launchconvert2.sh
+++ src/launchconvert2.sh
@@ -1,30 +1,75 @@
 #!/bin/bash
 
-LUSTREDIR=/lustre/isaac/proj/UTK0192/gensvc
+# LUSTREDIR=/lustre/isaac/proj/UTK0192/gensvc
+LUSTREDIR=/Users/johnmiller/data/gensvc/NovaSeqRuns
+logfile=/Users/johnmiller/data/gensvc/logs/submit.log
 BATCHNAME=bcl2fastq_v5.slurm
 FASTQDIR=./fastq.\$SLURM_JOB_ID
 MAILLIST=OIT_HPSC_Genomics@utk.edu,genomicscore@utk.edu,rkuster@utk.edu
 
-# for rundir in `dirname \`find "$LUSTREDIR" -maxdepth 3 -mtime -1 -name CopyComplete.txt\` 2>/dev/null`; do
-for rundir in `dirname \`find "$LUSTREDIR" -maxdepth 3 -name CopyComplete.txt\` 2>/dev/null`; do
-    echo "Checking: $rundir"
-    if [ `find "$rundir" -maxdepth 1 -name "$BATCHNAME" | wc -l` -eq 0 ]; then
-        # The batch script doesn't exist.
-        echo "No batch script found in $rundir"
-        if [ `find "$rundir/Alignment_1" -mindepth 2 -maxdepth 2 -type d -name Fastq | wc -l` -eq 0 ]; then
-            # It's a NovaSeq run.
-            echo "It's a NovaSeq run: $rundir"
-            # Make the batch script.
-            # Find the sample sheet.
-            # sbatch "$BATCHNAME"
-            # Send mail.
+
+find_sequencing_completed() {
+    dirname $(find "$LUSTREDIR" -maxdepth 3 -name CopyComplete.txt) | sort
+}
+
+
+submit() {
+    local rundir="$1"
+    submission=(sbatch myscript.slurm "$rundir")
+    echo "[DRYRUN] ${submission[@]}"
+}
+
+
+is_submitted() {
+    local rundir="$1"
+    grep "$rundir" "$logfile" | grep -q 'SUCCESS'
+}
+
+
+send_mail() {
+    local rundir="$1"
+    local jobid="$2"
+    local subject="Sequencing complete ($rundir)" 
+    local body="Conversion with bcl2fastq has been queued.\n
+\n
+jobid $jobid \n
+rundir $rundir\n
+\n
+You will receive additional mail from Slurm when the conversion job starts and when it completes."
+    _mail=(
+        foo
+        -s $subject
+        "$MAILLIST"
+        "$body"
+    )
+    printf "${_mail[@]}"
+}
+
+
+logger() {
+    local msg=("$@")
+    echo "$(date +%Y-%m-%dT%H:%M:%S) ${msg[@]}" | tee -a "$logfile"
+}
+
+
+main() {
+    mkdir -pv "$(dirname $logfile)"
+    for rundir in $(find_sequencing_completed) ; do
+        # echo $rundir
+        if is_submitted $rundir -eq 0 ; then
+            echo "Already submitted, nothing to do: $rundir"
         else
-            # It's a MiSeq run.
-            echo "It's a MiSeq run: $rundir"
-            # touch "$rundir/$BATCHNAME" # stop cron from repeatedly emailing about same job
-            # Send mail.
+            submit $rundir
+            if [[ $? -eq 0 ]] ; then
+                logger "SUCCESS Sequencing conversion job submitted to Slurm for run '$rundir'"
+            else
+                logger "ERROR Sequencing conversion failed for run '$rundir'"
+            fi
+            # send_mail $rundir JOBID
         fi
-    else
-        echo "Found batch script, therefore I assume the job is complete."
-    fi
-done
+    done
+}
+
+main
+
+exit 0
