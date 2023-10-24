87312a586ef448e053e3ea5a1492c7ac9f3152b6
izaakm <izaakm@users.noreply.github.com>
2023-10-24 11:52:55 -0400

    Initialize transfer module

---
+++ src/gensvc/misc/transfer.py
@@ -0,0 +1,27 @@
+def setup_transfer(seqrun=None, procdir=None, transfers=None, ignore_names=['Stats', 'Reports', 'all']):
+    # TODO: add dry_run
+    if seqrun:
+        # Set default values from seqrun.
+        procdir = procdir or seqrun.procdir
+        transfers = transfers or seqrun.procdir / 'transfers'
+
+    transfers = transfers or procdir / 'transfers'
+
+    for dir in procdir.iterdir():
+        if not dir.name in ['fastq', 'SummaryStatistics']:
+            continue
+            
+        for subdir in dir.iterdir():
+            # print(dir)
+            if not subdir.is_dir():
+                continue
+            elif subdir.name in ignore_names:
+                continue
+            print('source      :', subdir)
+        
+            project = subdir.name
+            group = subdir.parent.name
+            dest = transfers / project / group
+            print('destination :', dest)
+            # https://stackoverflow.com/a/10778930
+            # shutil.copytree(src, dst, copy_function=os.link)
