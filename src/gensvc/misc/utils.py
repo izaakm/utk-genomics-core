cb6e478aacbaa7ff5a63d362852fcafcae08d2d2
izaakm <izaakm@users.noreply.github.com>
2023-11-08 14:53:53 -0500

    Return dict of relevant env variables

--- src/gensvc/misc/utils.py
+++ src/gensvc/misc/utils.py
@@ -1,24 +1,32 @@
 import pathlib
 import os
+import re
 
 _get_path_or_none = lambda name: pathlib.Path(os.getenv(name)) if name in os.environ else None
 
-regex_runid = re.compile('[^\/]*\d{6}[^\/]*')
+_regex_runid = re.compile('[^\/]*\d{6}[^\/]*')
 
 def get_env():
+    env = dict()
     if 'GENSVC_DIR' in os.environ:
-        GENSVC_DIR = pathlib.Path(os.getenv('GENSVC_DIR'))
-        GENSVC_NOVASEQDATA = _get_path_or_none('GENSVC_NOVASEQDATA') or GENSVC_DIR / 'NovaSeqRuns'
-        GENSVC_MISEQDATA = _get_path_or_none('GENSVC_MISEQDATA') or GENSVC_DIR / 'MiSeqRuns'
-        GENSVC_PROCDATA = _get_path_or_none('GENSVC_PROCDATA') or GENSVC_DIR / 'processed'
+        env.update(
+            GENSVC_DIR = pathlib.Path(os.getenv('GENSVC_DIR')),
+            GENSVC_NOVASEQDATA = _get_path_or_none('GENSVC_NOVASEQDATA') or GENSVC_DIR / 'NovaSeqRuns',
+            GENSVC_MISEQDATA = _get_path_or_none('GENSVC_MISEQDATA') or GENSVC_DIR / 'MiSeqRuns',
+            GENSVC_PROCDATA = _get_path_or_none('GENSVC_PROCDATA') or GENSVC_DIR / 'processed'
+        )
     else:
-        GENSVC_DIR = None
-        GENSVC_NOVASEQDATA = None
-        GENSVC_MISEQDATA = None
-        GENSVC_PROCDATA = None
+        env.update(
+            GENSVC_DIR = None,
+            GENSVC_NOVASEQDATA = None,
+            GENSVC_MISEQDATA = None,
+            GENSVC_PROCDATA = None
+        )
+
+    return env
 
 def get_runid(path):
     try:
-        return regex_runid.search(str(path)).group(0)
+        return _regex_runid.search(str(path)).group(0)
     except:
         return None
