e1643cef2adfb8d429c11bda662e96dfbbac8b61
izaakm <izaakm@users.noreply.github.com>
2024-12-19 14:25:02 -0500

    fix: Update list function to handle new class properties

--- src/gensvc/misc/reports.py
+++ src/gensvc/misc/reports.py
@@ -1,5 +1,6 @@
 import re
 import pathlib
+import sys
 
 from gensvc.misc import sequencing_run, utils
 
@@ -19,7 +20,7 @@ def find_seq_runs(dirname):
 
     return sorted(seq_runs, key=lambda item: item[-1])
 
-def list(dirname, long=False):
+def list(dirname, long=False, sep='|'):
     short = not long
     if isinstance(dirname, str):
         dirname = pathlib.Path(dirname)
@@ -42,17 +43,35 @@ def list(dirname, long=False):
         # print(seqrun)
 
         if short:
-            if realpath == path:
-                print(instrument, runid, path)
-            else:
-                print(instrument, runid, path, '->', realpath)
+            # if realpath == path:
+            #     print(instrument, runid, path)
+            # else:
+            #     print(instrument, runid, path, '->', realpath)
+            print(sep.join([instrument, runid, str(path)]))
         elif long:
-            illuminadata = sequencing_run.IlluminaSequencingData(path)
             try:
-                for project in illuminadata.sample_project:
-                    print(f'{illuminadata.instrument:<8} {illuminadata.runid:<35} {illuminadata.info["Experiment Name"]:<40} {project:<40}')
-            except:
-                print(f'{instrument:<8} {runid:<35} {path}')
+                illuminadata = sequencing_run.IlluminaSequencingData(path)
+                illuminadata.find_samplesheet()
+                # print(illuminadata.path_to_samplesheet)
+                # print(illuminadata.samplesheet.path)
+                # --- 8<
+                # try:
+                #     for project in illuminadata.sample_project:
+                #         print(f'{illuminadata.instrument:<8} {illuminadata.runid:<35} {illuminadata.info["Experiment Name"]:<40} {project:<40}')
+                # except:
+                #     print(f'{instrument:<8} {runid:<35} {path}')
+                # --- >8
+                # print(f'{illuminadata.instrument:<8} {illuminadata.runid:<35} {illuminadata.info["Experiment Name"]:<40} {project:<40}')
+                # print(sep.join([illuminadata.instrument, illuminadata.runid, illuminadata.samplesheet.Header.get("Experiment Name"), project]))
+                for project in illuminadata.projects:
+                    # print(sep.join([illuminadata.instrument, illuminadata.runid, illuminadata.samplesheet.Header.get("Experiment Name"), project]))
+                    print(
+                        sep.join([
+                            str(i) for i in (illuminadata.instrument, illuminadata.runid, illuminadata.samplesheet.Header.get("Experiment Name"), project)
+                        ])
+                    )
+            except Exception as e:
+                print(f'[ERROR] Cannot processes run "{path}": {e}', file=sys.stderr)
     return None
 
 # END
