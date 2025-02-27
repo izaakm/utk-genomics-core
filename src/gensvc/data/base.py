356cb1991124cb39d161bb4b93eed496c4077912
izaakm <izaakm@email.com>
2025-02-27 09:08:43 -0500

    Move fxns into base module

---
+++ src/gensvc/data/base.py
@@ -0,0 +1,34 @@
+import pathlib
+
+
+class Datadir():
+    def __init__(self, path):
+        self._path = pathlib.Path(path)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}("{self.path}")'
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def realpath(self):
+        return self._path.resolve()
+
+    @property
+    def info(self):
+        return self.__dict__
+
+
+
+class RawData(Datadir):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class ProcessedData(Datadir):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
