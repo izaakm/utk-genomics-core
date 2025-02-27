6aefea81e0d54b6bfecb222010acc4d58d5f9027
izaakm <izaakm@email.com>
2025-02-27 08:38:03 -0500

    Comment out env vars from utils

--- src/gensvc/misc/utils.py
+++ src/gensvc/misc/utils.py
@@ -2,28 +2,27 @@ import pathlib
 import os
 import re
 
-_get_path_or_none = lambda name: pathlib.Path(os.getenv(name)) if name in os.environ else None
 
 _regex_runid = re.compile(r'[^\/]*\d{6}[^\/]*')
 
-def get_env():
-    env = dict()
-    if 'GENSVC_DIR' in os.environ:
-        env.update(
-            GENSVC_DIR = pathlib.Path(os.getenv('GENSVC_DIR')),
-            GENSVC_NOVASEQDATA = _get_path_or_none('GENSVC_NOVASEQDATA') or GENSVC_DIR / 'NovaSeqRuns',
-            GENSVC_MISEQDATA = _get_path_or_none('GENSVC_MISEQDATA') or GENSVC_DIR / 'MiSeqRuns',
-            GENSVC_PROCDATA = _get_path_or_none('GENSVC_PROCDATA') or GENSVC_DIR / 'processed'
-        )
-    else:
-        env.update(
-            GENSVC_DIR = None,
-            GENSVC_NOVASEQDATA = None,
-            GENSVC_MISEQDATA = None,
-            GENSVC_PROCDATA = None
-        )
-
-    return env
+# _get_path_or_none = lambda name: pathlib.Path(os.getenv(name)) if name in os.environ else None
+# def get_env():
+#     env = dict()
+#     if 'GENSVC_DIR' in os.environ:
+#         env.update(
+#             GENSVC_DIR = pathlib.Path(os.getenv('GENSVC_DIR')),
+#             GENSVC_NOVASEQDATA = _get_path_or_none('GENSVC_NOVASEQDATA') or GENSVC_DIR / 'NovaSeqRuns',
+#             GENSVC_MISEQDATA = _get_path_or_none('GENSVC_MISEQDATA') or GENSVC_DIR / 'MiSeqRuns',
+#             GENSVC_PROCDATA = _get_path_or_none('GENSVC_PROCDATA') or GENSVC_DIR / 'processed'
+#         )
+#     else:
+#         env.update(
+#             GENSVC_DIR = None,
+#             GENSVC_NOVASEQDATA = None,
+#             GENSVC_MISEQDATA = None,
+#             GENSVC_PROCDATA = None
+#         )
+#     return env
 
 def get_runid(path):
     try:
@@ -31,4 +30,8 @@ def get_runid(path):
     except:
         return None
 
-# END
+def as_path(obj):
+    try:
+        return pathlib.Path(obj)
+    except:
+        return None
