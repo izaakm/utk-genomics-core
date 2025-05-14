0fe1cddde959233b955a7d30a5f2d28eba2e8e51
izaakm <izaakm@email.com>
2025-05-14 12:01:42 -0400

    Refactor in progress

--- src/get-gensvc-data.sh
+++ src/get-gensvc-data.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
+set -u
+set -o pipefail
+
+declare logfile="logs/$(basename -s .sh "$0")-$(date +%s).log"
+
+# Redirect stdout and stderr to the logfile.
+mkdir -p logs
+exec 1>>"$logfile"
+exec 2>>"$logfile"
 
 declare dry_run=${DRY_RUN:-true}
 
@@ -11,7 +21,8 @@ declare logfile="${PROJECT_ROOT}/logs/get-gensvc-data.log"
 
 declare utk0192_data="/lustre/isaac24/proj/UTK0192/data"
 
-declare local_data="${GENSVC_DATADIR:-${SCRATCHDIR}/data/gensvc}"
+# declare local_data="${GENSVC_DATADIR:-${SCRATCHDIR}/data/gensvc}"
+declare local_data="${HOME}/data/raw/gensvc"
 
 # declare miseq_data="${utk0192_data}/MiSeqRuns"
 # declare local_miseq_data="${local_data}/MiSeq"
@@ -73,9 +84,73 @@ run echo "logging to ${logfile}"
 # dorsync "${iseq_data}" "${local_iseq_data}" &
 # wait
 
-dorsync "${iseq_data}" "${local_iseq_data}"
-dorsync "${nextseq_data}" "${local_nextseq_data}"
-dorsync "${novaseq_data}" "${local_novaseq_data}"
-dorsync "${processed_data}" "${local_processed_data}"
+# dorsync "${iseq_data}" "${local_iseq_data}"
+# dorsync "${nextseq_data}" "${local_nextseq_data}"
+# dorsync "${novaseq_data}" "${local_novaseq_data}"
+# dorsync "${processed_data}" "${local_processed_data}"
+
+include_files() {
+    echo "Illumina/"
+    echo "processed/"
+}
+
+# run rsync \
+#     --dry-run \
+#     -auv \
+#     --no-owner \
+#     --no-perms \
+#     --files-from=<(include_files) \
+#     --recursive \
+#     --exclude='.Trash*' \
+#     --exclude='.git' \
+#     --exclude='*.bin' \
+#     --exclude='*.bcl' \
+#     --exclude='*.cbcl' \
+#     --exclude='*.imf1' \
+#     --exclude='*.filter' \
+#     --exclude='*.tif' \
+#     --exclude='*.fastq.gz' \
+#     --exclude='*.incomplete' \
+#     --exclude='Firmware' \
+#     --exclude='Thumbnail_Images' \
+#     --delete-excluded \
+#     "jmill165@dtn1.isaac.utk.edu:${utk0192_data}/" "${local_data}/"
+
+# run rsync \
+#     --dry-run \
+#     -auv \
+#     --no-owner \
+#     --no-perms \
+#     --include="*/" \
+#     --include="*.csv" \
+#     --include="*.txt" \
+#     --exclude="*" \
+#     --delete-excluded \
+#     "jmill165@dtn1.isaac.utk.edu:${utk0192_data}/" "${local_data}/"
+
+run rsync \
+    --dry-run \
+    -au \
+    -vv \
+    --no-owner \
+    --no-perms \
+    --exclude='.Trash*' \
+    --exclude='.git' \
+    --exclude='*.bin' \
+    --exclude='*.bcl' \
+    --exclude='*.cbcl' \
+    --exclude='*.imf1' \
+    --exclude='*.filter' \
+    --exclude='*.tif' \
+    --exclude='*.fastq.gz' \
+    --exclude='*.incomplete' \
+    --exclude='Firmware' \
+    --exclude='Thumbnail_Images' \
+    --delete-excluded \
+    "jmill165@dtn1.isaac.utk.edu:${utk0192_data}/" "${local_data}/"
+
+
+
+
 
 exit 0
