6f119b2663e6a46dee8e6fc70ca0baa7d560b846
izaakm <izaakm@users.noreply.github.com>
2025-06-30 13:36:05 -0400

    Add template makefile for setting up Globus transfers

---
+++ src/templates/transfer-to-globus.mk
@@ -0,0 +1,34 @@
+SHELL := /bin/bash
+
+UTK0192 := /lustre/isaac24/proj/UTK0192
+GLOBUS_DATA := $(UTK0192)/data/globus
+PROCESSED_DATA := $(UTK0192)/data/processed
+
+runid := $(shell basename $(realpath ..))
+sample_project := $(shell find external unknown_project -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
+$(info $(sample_project))
+
+globus_projects := $(addprefix $(GLOBUS_DATA)/$(runid)/,$(sample_project))
+collections := $(GLOBUS_DATA)/$(runid)/COLLECTIONS
+
+# $(info $(runid))
+# $(info $(sample_project))
+# $(info $(globus_projects))
+
+all: $(globus_projects)
+
+$(GLOBUS_DATA)/$(runid)/%: $(PROCESSED_DATA)/$(runid)/transfer/external/% | $(GLOBUS_DATA)/$(runid) $(collections)
+	@echo "External: $(*)"
+	cp -lr $(<) $(@D)/
+	echo "$(runid) - $(*)" >> $(@D)/COLLECTIONS
+
+$(GLOBUS_DATA)/$(runid)/%: $(PROCESSED_DATA)/$(runid)/transfer/unknown_project/% | $(GLOBUS_DATA)/$(runid) $(collections)
+	@echo "Unknown Project: $(*)"
+	cp -lr $(<) $(@D)/
+	echo "$(runid) - $(*)" >> $(@D)/COLLECTIONS
+
+$(collections):
+	@echo "<PI Last Name> UTK Illumina Data <YYYYMMDD> [(<Collaborator Last Name>)]" > $(@)
+
+$(GLOBUS_DATA)/$(runid):
+	test -d $(GLOBUS_DATA) && mkdir -p $(@)
