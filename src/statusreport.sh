9eaa57fad773a055b66583eaf159f1e402f6c46f
izaakm <izaakm@email.com>
2025-07-10 17:58:54 -0400

    refactor: Print each transfer status item on a separate line

--- src/statusreport.sh
+++ src/statusreport.sh
@@ -5,9 +5,10 @@ set -e
 set -u
 set -o pipefail
 
-declare datadir="/Users/jmill165/data/raw/gensvc"
-declare illumina_dir="${datadir}/Illumina"
-declare proc_data="${datadir}/processed"
+declare -r datadir="/Users/jmill165/data/raw/gensvc"
+declare -r illumina_data="${datadir}/Illumina"
+declare -r proc_data="${datadir}/processed"
+declare -r globus_data="${datadir}/globus"
 declare -a run_dirs=( ${datadir}/Illumina/*Runs/*{A01770,FS10003266,VL00838}* )
 declare -i width=36
 
@@ -15,16 +16,16 @@ declare -i width=36
 
 copy_status() {
     local runid="$1"
-    local run_dir=$(find ${illumina_dir}/*Runs -maxdepth 1 -type d -name ${runid} -print)
+    local run_dir=$(find ${illumina_data}/*Runs -maxdepth 1 -type d -name ${runid} -print)
     local completed="${run_dir}/CopyComplete.txt"
+    local fmtstr="%-${width}s | 1) %s\n"
     # echo ${completed}
-    # test -f $completed
     if [[ -f $completed ]]; then
         # echo "${runid} | Raw data: completed"
-        printf "%-${width}s | %s\n" "${runid}" "Raw data: completed"
+        printf "$fmtstr" "${runid}" "Raw data | completed"
     else
         # echo "${runid} | Raw data: INCOMPLETE"
-        printf "%-${width}s | %s\n" "${runid}" "Raw data: INCOMPLETE"
+        printf "$fmtstr" "${runid}" "Raw data | INCOMPLETE"
     fi
 }
 
@@ -34,20 +35,17 @@ bclconvert_status() {
     local completed="${bclconvert_dir}/Logs/FastqComplete.txt"
     local inprogress="${bclconvert_dir}.inprogress"
     local submitted="${bclconvert_dir}/BCLConvertSubmitComplete"
+    local fmtstr="%-${width}s | 2) %s\n"
     if [[ -f $inprogress ]] ; then
         # Check 'in progress' first. It's possible that a 'completed' file exists from a previous run.
-        # echo "${runid} | BCLConvert: IN PROGRESS"
-        printf "%-${width}s | %s\n" "${runid}" "BCLConvert: IN PROGRESS"
+        printf "$fmtstr" "${runid}" "BCLConvert | IN PROGRESS"
     elif [[ -f $completed ]] ; then
         # Then check for completion.
-        # echo "${runid} | BCLConvert: completed"
-        printf "%-${width}s | %s\n" "${runid}" "BCLConvert: completed"
+        printf "$fmtstr" "${runid}" "BCLConvert | completed"
     elif [[ -f $submitted ]] ; then
-        # echo "${runid} | BCLConvert: SUBMITTED"
-        printf "%-${width}s | %s\n" "${runid}" "BCLConvert: SUBMITTED"
+        printf "$fmtstr" "${runid}" "BCLConvert | SUBMITTED"
     else
-        # echo "${runid} | BCLConvert: NOT STARTED"
-        printf "%-${width}s | %s\n" "${runid}" "BCLConvert: NOT STARTED"
+        printf "$fmtstr" "${runid}" "BCLConvert | NOT STARTED"
     fi
 }
 
@@ -56,56 +54,89 @@ transfer_status() {
     # <processed>/<runid>/transfer/<destination>/<sample_project>
     local runid="$1"
     local -a sample_project_dirs=( $(find ${proc_data}/${runid}/transfer/*/* -maxdepth 0 -type d -print 2>/dev/null) )
+    local fmtstr="%-${width}s | 3) %s\n"
     local sample_project_dir
     local sample_project_name
     local transfer_complete
     if [[ ${#sample_project_dirs[@]} -eq 0 ]]; then
-        # echo "${runid} | Transfer: NOT STARTED"
-        printf "%-${width}s | %s\n" "${runid}" "Transfer: NOT STARTED"
+        printf "$fmtstr" "${runid}" "Transfer | NOT STARTED"
         return
     fi
     for sample_project_dir in "${sample_project_dirs[@]}"; do
+        ncomp=0
         sample_project_name=$(basename "${sample_project_dir}")
         notification_complete="${sample_project_dir}.EmailNotification.txt"
         # echo "Sample Project Dir: ${sample_project_dir}"
         if [[ $sample_project_name == *globus* ]] ; then
             transfer_complete="${sample_project_dir}.GlobusTransferComplete"
-            collection_complete="${sample_project_dir}.GlobusCollectionComplete"
-            printf "%-${width}s | %s\n" "${runid}" "Globus Transfer ${sample_project_name}"
-        elif [[ $sample_project_name == *external* ]] ; then
-            transfer_complete="${sample_project_dir}.GlobusTransferComplete"
-            collection_complete="${sample_project_dir}.GlobusCollectionComplete"
+            collection_complete="${globus_data}/${runid}/${sample_project_name}.collection.json"
             if [[ -f "$notification_complete" ]] ; then
-                printf "%-${width}s | %s: All Complete\n" "${runid}" "External Transfer ${sample_project_name}"
-            elif [[ -f "$collection_complete" ]] ; then
-                # echo "${runid} | External Transfer ${sample_project_name}: Collection Complete"
-                printf "%-${width}s | %s: Collection Complete\n" "${runid}" "External Transfer ${sample_project_name}"
-            elif [[ -f "$transfer_complete" ]] ; then
-                # echo "${runid} | External Transfer ${sample_project_name}: Transfer Complete"
-                printf "%-${width}s | %s: Transfer Complete\n" "${runid}" "External Transfer ${sample_project_name}"
+                ((ncomp+=1))
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Notification Complete"
             else
-                # echo "${runid} | External Transfer ${sample_project_name}: INCOMPLETE"
-                printf "%-${width}s | %s: INCOMPLETE\n" "${runid}" "External Transfer ${sample_project_name}"
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Notification INCOMPLETE"
             fi
+            if [[ -f "$collection_complete" ]] ; then
+                ((ncomp+=1))
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Collection Complete"
+            else
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Collection INCOMPLETE"
+            fi
+            if [[ -f "$transfer_complete" ]] ; then
+                ((ncomp+=1))
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Transfer Complete"
+            else
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Transfer INCOMPLETE"
+            fi
+            printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | ${ncomp} of 3 complete"
+        elif [[ $sample_project_dir == *external* ]] ; then
+            transfer_complete="${sample_project_dir}.GlobusTransferComplete"
+            collection_complete="${globus_data}/${runid}/${sample_project_name}.collection.json"
+            if [[ -f "$notification_complete" ]] ; then
+                ((ncomp+=1))
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Notification Complete"
+            else
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Notification INCOMPLETE"
+            fi
+            if [[ -f "$collection_complete" ]] ; then
+                ((ncomp+=1))
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Collection Complete"
+            else
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Collection INCOMPLETE"
+            fi
+            if [[ -f "$transfer_complete" ]] ; then
+                ((ncomp+=1))
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Transfer Complete"
+            else
+                printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | Transfer INCOMPLETE"
+            fi
+            printf "$fmtstr" "${runid}" "External Transfer ${sample_project_name} | ${ncomp} of 3 complete"
         elif [[ $sample_project_name == UTK* ]] ; then
             transfer_complete="${sample_project_dir}.ISAACTransferComplete"
             if [[ -f "$notification_complete" ]] ; then
-                printf "%-${width}s | %s: All Complete\n" "${runid}" "ISAAC Transfer ${sample_project_name}"
-            elif [[ -f "$transfer_complete" ]] ; then
-                # echo "${runid} | ISAAC Transfer ${sample_project_name}: Transfer Complete"
-                printf "%-${width}s | %s: Transfer Complete\n" "${runid}" "ISAAC Transfer ${sample_project_name}"
+                ((ncomp+=1))
+                printf "$fmtstr" "${runid}" "ISAAC Transfer ${sample_project_name} | Notification Complete"
             else
-                # echo "${runid} | ISAAC Transfer ${sample_project_name}: INCOMPLETE"
-                printf "%-${width}s | %s: INCOMPLETE\n" "${runid}" "ISAAC Transfer ${sample_project_name}"
+                printf "$fmtstr" "${runid}" "ISAAC Transfer ${sample_project_name} | Notification INCOMPLETE"
             fi
+            if [[ -f "$transfer_complete" ]] ; then
+                ((ncomp+=1))
+                printf "$fmtstr" "${runid}" "ISAAC Transfer ${sample_project_name} | Transfer Complete"
+            else
+                printf "$fmtstr" "${runid}" "ISAAC Transfer ${sample_project_name} | Transfer INCOMPLETE"
+            fi
+            printf "$fmtstr" "${runid}" "ISAAC Transfer ${sample_project_name} | ${ncomp} of 2 complete"
         else
-            # echo "${runid} | Transfer ${sample_project_dir}"
-            printf "%-${width}s | %s\n" "${runid}" "Transfer ${sample_project_name}"
+            printf "$fmtstr" "${runid}" "Transfer ${sample_project_name} | UNKNOWN TRANSFER TYPE"
         fi
     done
 }
 
 for run_dir in "${run_dirs[@]}"; do
+    if [[ -f "${run_dir}/NOOP" ]] ; then
+        echo "Skipping ${run_dir} due to NOOP file." 1>&2
+        continue
+    fi
     runid=$(basename "${run_dir}")
     copy_status "${runid}"
     bclconvert_status "${runid}"
