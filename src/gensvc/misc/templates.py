5aca86b90178f073ff8027e7be42d2b8c8db6f36
izaakm <izaakm@email.com>
2025-09-05 09:43:04 -0400

    Add templates module

---
+++ src/gensvc/misc/templates.py
@@ -0,0 +1,68 @@
+templates = {}
+templates['archive.sh'] = '''
+#!/bin/bash -l
+#SBATCH --job-name={__job_name}
+#SBATCH --account=ISAAC-UTK0192
+#SBATCH --partition=short
+#SBATCH --qos=short
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=0-03:00:00
+#SBATCH --chdir {__utstor_dir}
+#SBATCH --output=%j-%x.o
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=OIT_HPSC_Genomics@utk.edu
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+umask 002
+
+declare rundir="{__rundir}"
+declare runid="{__runid}"
+declare utstor_dir="{__utstor_dir}"
+
+declare tar_file="${{utstor_dir}}/${{runid}}.tar"
+declare archive_complete="${{utstor_dir}}/${{runid}}.archive_complete"
+
+logger() {{
+    local content=("$@")
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] ${{content[@]}}"
+}}
+
+cd "$utstor_dir"
+
+# If the target already exists, exit.
+test -f "${{archive_complete}}" && exit 1
+test -f "${{runid}}.inprogress" && exit 1
+test -f "$runid" && exit 1
+
+logger "Make hardlinks to the data ..."
+cp -lr "$rundir" "./${{runid}}.inprogress"
+mv -n "./${{runid}}.inprogress" "./${{runid}}"
+
+logger "Calculate checksums ..."
+cd "$runid"
+find . -type f -not -name 'checksums.sha256*' -print0 | xargs -0 -L1 --max-procs 0 sha256sum > "checksums.sha256.inprogress"
+mv -n "checksums.sha256.inprogress" "checksums.sha256"
+cd -
+
+logger "Compress the data ..."
+tar --exclude='*.fastq.gz' -cf "${{tar_file}}.inprogress" "${{runid}}"
+mv -n "${{tar_file}}.inprogress" "${{tar_file}}"
+
+logger "Also checksum the tar file ..."
+sha256sum "$(basename "${{tar_file}}")" > "${{tar_file}}.sha256.inprogress"
+mv -n "${{tar_file}}.sha256.inprogress" "${{tar_file}}.sha256"
+
+logger "Cleaning up ..."
+mv -n "$runid" "${{runid}}.archivecomplete"
+# touch ArchiveSubmitComplete
+
+logger "All complete."
+
+exit 0
+'''
+
