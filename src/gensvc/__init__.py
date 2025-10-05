53f1de9dbc7b185ef8c9178bc50d016f6ec4a1bd
izaakm <izaakm@email.com>
2025-10-04 20:51:33 -0400

    Update log message format

--- src/gensvc/__init__.py
+++ src/gensvc/__init__.py
@@ -4,7 +4,7 @@ logger = logging.getLogger('root')
 
 # Sets the correct format and only one logger ....
 logging.basicConfig(
-    format='[%(asctime)s] %(name)s %(levelname)s - %(message)s',
+        format='[%(asctime)s] %(levelname)s from %(name)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
