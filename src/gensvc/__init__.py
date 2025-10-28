7db2907c91fff618320993454a6d7a25e084d45e
izaakm <izaakm@email.com>
2025-10-28 07:51:27 -0400

    refactor: fix typo

--- src/gensvc/__init__.py
+++ src/gensvc/__init__.py
@@ -4,7 +4,7 @@ logger = logging.getLogger('root')
 
 # Sets the correct format and only one logger ....
 logging.basicConfig(
-        format='[%(asctime)s] %(levelname)s from %(name)s: %(message)s',
+    format='[%(asctime)s] %(levelname)s from %(name)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
