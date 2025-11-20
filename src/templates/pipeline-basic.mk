5d15839711ad8cdb63f7e2ccced8f2ec000201ed
jmill165 <jmill165@noemail.com>
2025-11-20 16:35:07 -0500

    refactor: Find files in fastq directory

--- src/templates/pipeline-basic.mk
+++ src/templates/pipeline-basic.mk
@@ -6,13 +6,13 @@ SLURM_ACCOUNT ?= acf-utk0011
 SLURM_PARTITION ?= short
 SLURM_QOS ?= short
 SLURM_CPUS_PER_TASK ?= 16
+SLURM_TIMELIMIT ?= 03:00:00
 
 SRUN := srun \
         --nodes 1 \
         --ntasks 1 \
         --cpus-per-task $(SLURM_CPUS_PER_TASK) \
-        --export=ALL,LC_ALL=C \
-        --time 03:00:00
+        --export=ALL,LC_ALL=C
 
 ifdef SLURM_JOB_ID
 SRUN += --exclusive
@@ -21,6 +21,7 @@ SRUN += --job-name qc-$(notdir $(PWD)) \
         --account $(SLURM_ACCOUNT) \
         --partition $(SLURM_PARTITION) \
         --qos $(SLURM_QOS) \
+        --time $(SLURM_TIMELIMIT) \
         --mail-user \
         --mail-type=END,FAIL
 endif
@@ -34,7 +35,7 @@ FASTQC_FILES := $(patsubst fastq/%.fastq.gz,fastqc/%.html,$(FASTQ_FILES))
 all: checksums.sha256 $(FASTQC_FILES)
 
 checksums.sha256: $(FASTQ_FILES)
-	$(SRUN) bash -c 'find $(^) -print0 | xargs -0 -I {} -P0 sha256sum {} > $(@)'
+	$(SRUN) bash -c 'find fastq -type f -name "*.fastq.gz" -print0 | xargs -0 -I {} -P0 sha256sum {} > $(@)'
 
 .PHONY: fastqc
 fastqc: $(FASTQC_FILES)
