2ae4b4e8452cdf1e055738ad623da033db37d7bf
jmill165 <jmill165@noemail.com>
2025-11-20 15:20:16 -0500

    refactor: Update basic pipeline for processing sequencing run results

--- src/templates/pipeline-basic.mk
+++ src/templates/pipeline-basic.mk
@@ -5,23 +5,24 @@ SHELL := /bin/bash
 SLURM_ACCOUNT ?= acf-utk0011
 SLURM_PARTITION ?= short
 SLURM_QOS ?= short
-CPUS_PER_TASK ?= 16
+SLURM_CPUS_PER_TASK ?= 16
 
 SRUN := srun \
-		--account $(SLURM_ACCOUNT) \
-		--partition $(SLURM_PARTITION) \
-		--qos $(SLURM_QOS) \
-		--ntasks 1 \
-		--cpus-per-task $(CPUS_PER_TASK) \
-		--export=ALL,LC_ALL=C \
-		--time 03:00:00
+        --nodes 1 \
+        --ntasks 1 \
+        --cpus-per-task $(SLURM_CPUS_PER_TASK) \
+        --export=ALL,LC_ALL=C \
+        --time 03:00:00
 
 ifdef SLURM_JOB_ID
 SRUN += --exclusive
 else
 SRUN += --job-name qc-$(notdir $(PWD)) \
-		--mail-user \
-		--mail-type=END,FAIL
+        --account $(SLURM_ACCOUNT) \
+        --partition $(SLURM_PARTITION) \
+        --qos $(SLURM_QOS) \
+        --mail-user \
+        --mail-type=END,FAIL
 endif
 
 FASTQC := apptainer exec 'https://depot.galaxyproject.org/singularity/fastqc:0.12.1--hdfd78af_0' fastqc
@@ -39,4 +40,4 @@ checksums.sha256: $(FASTQ_FILES)
 fastqc: $(FASTQC_FILES)
 fastqc/%.html: fastq/%.fastq.gz
 	mkdir -p fastqc
-	$(SRUN) $(FASTQC) --threads $(CPUS_PER_TASK) -o fastqc $(<)
+	$(SRUN) $(FASTQC) --threads $(SLURM_CPUS_PER_TASK) -o fastqc $(<)
